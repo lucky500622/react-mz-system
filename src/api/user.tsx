a43ba4cece4c8512dab9c48c393bc09ed4025ef8
ljy <m18318859136@163.com>
2026-05-24 16:30:00 +0800

    api接口调整

--- src/api/user.tsx
+++ src/api/user.tsx
@@ -1,18 +1,32 @@
 import request from '@/utils/request'
 
 // 用户注册
+type RegisterResponse = {
+  code: number;
+  msg: string;
+  data: {
+    user_name: string;
+  }
+}
 export const registerService = (data: {
   user_name: string;
   user_password: string;
 }) => {
-  return request.post('/user/register', data)
+  return request.post('/user/register', data) as Promise<RegisterResponse>
 }
 
 // 用户名查重接口
+type CheckUsernameResponse = {
+  code: number;
+  msg: string;
+  data: {
+    isExist: boolean;
+  }
+}
 export const checkUsernameService = (data: {
   user_name: string;
 }) => {
-  return request.get('/user/register/checkUsername', { params: data })
+  return request.get('/user/register/checkUsername', { params: data }) as Promise<CheckUsernameResponse>
 }
 
 // 用户登录
